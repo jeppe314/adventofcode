6a6da9aa42a75556a8b2a1e01b024fd78c209c2a
jeppe314 <jesperlindqvist93@gmail.com>
2022-12-09 19:23:46 +0100

    finished 4-2

---
+++ 4/two.js
@@ -0,0 +1,29 @@
+const { input } = require("./input")
+
+let pairs = []
+
+for (let i = 0; i < input.length; i++) {
+    let temp = []
+
+    let str = input[i].split(",")
+
+    for (let j = 0; j < str.length; j++) {
+        temp.push(...str[j].split("-"))
+    }
+    temp = temp.map((x) => parseInt(x))
+    pairs.push(temp)
+}
+
+let overlapping = 0
+
+pairs.forEach((pair) => {
+    if (
+        (pair[0] >= pair[2] && pair[0] <= pair[3]) ||
+        (pair[1] >= pair[2] && pair[1] <= pair[3]) ||
+        (pair[2] >= pair[0] && pair[2] <= pair[1])
+    ) {
+        overlapping += 1
+    }
+})
+
+console.log(overlapping)
